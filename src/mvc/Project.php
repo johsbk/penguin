6918b12e927d7168446192de2b6c6cf8a9ceea3d
Johs Kristoffersen <johsbk@gmail.com>
2015-03-10 16:00:32 +0100

    test

--- src/mvc/Project.php
+++ src/mvc/Project.php
@@ -75,7 +75,7 @@ abstract class Project {
 	abstract function inittwig($twig);
 	static function autoload($class_name) {
 		$filename = $class_name . '.php';
-		$t = str_replace("\\",'/',SITE_PATH.'/'.$filename);
+		$t = str_replace("\\",'/',SITE_PATH.'/src/'.$filename);
 		if (file_exists($t)) {
 			include($t);
 			return true;
