c8c85dd861e50efdd65c235c06d7cff2c78f058f
Johs Kristoffersen <johsbk@gmail.com>
2015-04-29 10:36:49 +0200

    test

--- src/jobs/Job.php
+++ src/jobs/Job.php
@@ -1,10 +1,11 @@
 <?php
 namespace penguin\jobs;
 abstract class Job {
-	public $created_at = new \DateTime();
+	public $created_at;
 	abstract function run();
 
 	function create() {
+		$this->created_at = new \DateTime();
 		$jq = JobQueue::getInstance();
 		$jq->add($this);
 	}
